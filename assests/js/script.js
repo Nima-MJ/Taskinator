e939aa00ddd7d5de37d00da55e2346bf5817a795
Nima Mehrjoo <nmehrjoo1999@gmail.com>
2021-11-30 10:55:45 -0500

    nima is a bitch

--- assests/js/script.js
+++ assests/js/script.js
@@ -89,17 +89,35 @@ var createTaskActions = function(taskId){
 formEl.addEventListener("submit", taskFormHandler);
 
 var taskButtonHandler = function(event){
-    console.log(event.target);
-    if (event.target.matches(".delete-btn")){
+    //get target element from event 
+    var targetEl = event.target; //event.target allows you to read where the user clicks
+
+    if (targetEl.matches(".edit-btn")){
+        var taskId = targetEl.getAttribute("data-task-id");
+        editTask(taskId);
+    }
+    else if (targetEl.matches(".delete-btn")){
         console.log("you clicked a delete button!")
         var taskId = event.target.getAttribute("data-task-id");
         deleteTask(taskId);
     }
+
 };
 
 var deleteTask = function(taskId){
     console.log(taskId);
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
     taskSelected.remove();
+};
+var editTask = function (taskId){
+    //get task list item element 
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    console.log(taskSelected);
+    //get content from task name and type 
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    console.log(taskName);
+
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+    console.log(taskType);
 };
 pageContentEl.addEventListener("click", taskButtonHandler);
